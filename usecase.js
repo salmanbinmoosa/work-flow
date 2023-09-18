b7cbb8ddfbefb6ecdf2bb0e6f8bb826c3cab4547
Salman Bin Moosa <salmanbinmoosaa@gmail.com>
2023-09-18 19:02:48 +0530

    another pages added

--- usecase.js
+++ usecase.js
@@ -1,45 +1,45 @@
 
-// add new list toggle starts //
-
-const hideUSeCase = document.getElementById("hide-use-case")
-const addNewListBtn = document.getElementById("add-new-list-btn")
-
-hideUSeCase.addEventListener("click", () => {
-    hideUSeCase.style.display = "none"
-    console.log("add new use btn Clicked");
-
-    addNewListBtn.style.display = "flex"
-})
-
-// add new list toggle ends //
-
-
-// on click new element //
+// adding elements in js 
 const listsParent = document.getElementById("lists-parent")
 const addList = document.getElementById("add-list")
 const listInput = document.getElementById("listInput")
 const cloneUsecase = document.getElementById("clone-usecase")
-addList.addEventListener("click", () => {
-    if (listInput.value !== "") {
-        addNewListBtn.style.display = "none"
 
 
-        const newElement = document.createElement("div")
-        listsParent.appendChild(newElement)
-        newElement.classList.add("list")
+const hideUSeCase = document.getElementById("hide-use-case")
+const addNewListBtn = document.getElementById("add-new-list-btn")
+// added elements
 
-        const listHtwo = document.createElement("h2")
-        newElement.appendChild("listHtwo")
-        listHtwo.innerText = "new element"
+// adding eventin add use case tpggle
+hideUSeCase.addEventListener("click", () => {
+    hideUSeCase.style.display = "none"
+    addNewListBtn.style.display = "flex"
+
+    addList.addEventListener("click", () => {
+
+        if (listInput.value !== "") {
+
+            addNewListBtn.style.display = "none"
+
+            const listTempelete = document.getElementById("list-tempelete")
+            const cloneTempelete = listTempelete.cloneNode(true)
+
+            listsParent.appendChild(cloneTempelete);
+
+            listsParent.appendChild(hideUSeCase)
+            listsParent.appendChild(addNewListBtn)
+        
+            hideUSeCase.style.display = "flex"
+
+            const tempH2 = cloneTempelete.querySelector("#temp-h2");
+            tempH2.textContent = listInput.value
 
 
-    }
-
+        }
+    })
 })
 
-addList.addEventListener("click", () => {
 
-    const clonedUsecase = cloneUsecase.cloneNode(true);
-    listsParent.appendChild(clonedUsecase)
-    return clonedUsecase
-})
+// const cloneHideUSeCase = hideUSeCase.cloneNode(true)
+// listsParent.appendChild(cloneHideUSeCase)
+// cloneHideUSeCase.style.display = "flex"
